1a598d27b550f83e0f971cca4e9387266f59b9d7
srinskit <srinskit@users.noreply.github.com>
2020-03-22 21:34:50 +0530

    Added shape, padding, debug selector, Added rounder

--- squarepants/squarepants/main.py
+++ squarepants/squarepants/main.py
@@ -1,6 +1,7 @@
 #! python3
 import cv2
 import argparse
+import numpy as np
 
 
 def main():
@@ -9,12 +10,40 @@ def main():
         "image",
         help="Path to image file"
     )
+
+    parser.add_argument(
+        "-s",
+        "--shape",
+        choices=["original", "round"],
+        default="original",
+        help="Shaped of the inner image"
+    )
+
+    parser.add_argument(
+        "-p",
+        "--padding",
+        default=40,
+        type=int,
+        help="Minimum padding around the inner image"
+    )
+
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Run in debug mode"
+    )
+
     args = parser.parse_args()
     img = cv2.imread(args.image, cv2.IMREAD_COLOR)
 
     h, w, _ = img.shape
-    pad, sample_p, blur = 40, 90, 70
-    sq_hw = max(h, w) + 2*pad
+    pad, sample_p, blur = args.padding, 90, 70
+
+    if args.shape == "round":
+        sq_hw = min(h, w) + 2*pad
+    elif args.shape == "original":
+        sq_hw = max(h, w) + 2*pad
 
     final_h, final_w = sq_hw, sq_hw
     sample_h, sample_w = min(h, w), min(h, w)
@@ -32,10 +61,29 @@ def main():
     res = cv2.GaussianBlur(res, (k_size, k_size), 0)
 
     r_h, r_w, _ = res.shape
-    assert r_h >= h and r_w >= w, "result image smaller"
-    res[r_h//2-h//2: r_h//2-h//2+h,
-        r_w//2-w//2: r_w//2-w//2+w] = img
+    assert r_h == final_h and r_w == final_w, ":(("
 
+    if args.shape == "round":
+        r = min(h, w)//2
+        img = img[h//2-r: h//2+r, w//2-r:w//2+r]
+        mask = np.zeros((2*r, 2*r), np.uint8)
+        mask = cv2.circle(mask, (r, r), r, 255, cv2.FILLED)
+
+        img = cv2.bitwise_and(img, img, mask=mask)
+        x = res[r_h//2-r: r_h//2+r,
+                r_w//2-r: r_w//2+r]
+
+        x = cv2.bitwise_and(x, x, mask=cv2.bitwise_not(mask))
+        img += x
+        res[r_h//2-r: r_h//2+r,
+            r_w//2-r: r_w//2+r] = img
+    elif args.shape == "original":
+        res[r_h//2-h//2: r_h//2-h//2+h,
+            r_w//2-w//2: r_w//2-w//2+w] = img
+
+    if args.debug:
+        cv2.imshow("res", res)
+        cv2.waitKey(0)
     cv2.imwrite("sq_"+args.image, res)
 
 
